2b517013b13e13856728dcb83013fe818136148a
Dmitrii Vokuev <>
2020-11-24 16:34:36 +0100

    commit with code

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void studentTest(){
+        System.out.print("Hello student!\n");
+    }
+
+    @Test
+    public void teacherTest(){
+        System.out.print("Hello teacher!\n");
+    }
+
+    @Test
+    public void lessonTest(){
+        String methodName = new Object() {}
+                .getClass()
+                .getEnclosingMethod()
+                .getName();
+        System.out.print(methodName + " for teacher & student\n");
+    }
+
+    @Test
+    public void paramTest(){
+        String teacherName = System.getProperty("teacherName");
+        System.out.print(teacherName + " , hi! \n");
+    }
+}
